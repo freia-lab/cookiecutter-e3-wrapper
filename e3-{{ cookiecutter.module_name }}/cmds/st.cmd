91fcf9fdff4fc97d0d93474563d4f29890b26b3d
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2021-10-28 14:40:53 +0200

    modify template files

--- e3-{{ cookiecutter.module_name }}/cmds/st.cmd
+++ e3-{{ cookiecutter.module_name }}/cmds/st.cmd
@@ -1,4 +1,3 @@
 # This should be a test or example startup script
-require {{ cookiecutter.module_name }}
 
-iocshLoad("$({{ cookiecutter.module_name }}_DIR)/{{ cookiecutter.module_name }}.iocsh")
+require {{ cookiecutter.module_name }}
